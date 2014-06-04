e048074d09e381d5e799f865f445350b72f7d55a
seny <arseny.vakhrushev@gmail.com>
2014-06-04 13:43:13 -0700

    + Fix copyright years

--- src/amf3.c
+++ src/amf3.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2012-2013 Arseny Vakhrushev <arseny.vakhrushev at gmail dot com>
+** Copyright (C) 2012-2014 Arseny Vakhrushev <arseny.vakhrushev at gmail dot com>
 ** Please read the LICENSE file for license details
 */
 
@@ -17,7 +17,7 @@ static const struct luaL_Reg amf3_lib[] = {
 int luaopen_amf3(lua_State *L) {
 	luaL_register(L, "amf3", amf3_lib);
 	lua_pushliteral(L, "_COPYRIGHT");
-	lua_pushliteral(L, "Copyright (C) 2012-2013 Arseny Vakhrushev");
+	lua_pushliteral(L, "Copyright (C) 2012-2014 Arseny Vakhrushev");
 	lua_settable(L, -3);
 	lua_pushliteral(L, "_DESCRIPTION");
 	lua_pushliteral(L, "AMF3 encoding/decoding library for Lua");
